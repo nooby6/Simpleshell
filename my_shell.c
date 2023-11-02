0fb7d1ad6124e8cdeead4972759275f91be2c980
nooby6 <giftaudrine1@gmail.com>
2023-11-02 03:04:38 -0700

    my_shell.c

--- my_shell.c
+++ my_shell.c
@@ -5,72 +5,97 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(void)
+/**
+ * Entry point
+ * *custom_getline - Function to read a line of input 
+ * Return 0: Success
+ *
+ */
+char *custom_getline(void)
 {
-    char *input = NULL;
-    size_t bufsize = 0;
-    ssize_t characters;
+    static char buffer[1024]; /* Static buffer to read input */
+    memset(buffer, 0, sizeof(buffer)); /* Clear the buffer */
 
+    char c;
+    int index = 0;
+
+    /* Read characters until a newline or the end of the file */
     while (1)
     {
-        printf("$ "); /* Display the prompt */
-        characters = getline(&input, &bufsize, stdin);
+        c = getchar();
 
-        if (characters == EOF) /* Check for end of file (Ctrl+D) */
+        if (c == EOF || c == '\n')
         {
-            printf("\n");
-            free(input);
-            exit(0);
+            buffer[index] = '\0'; /* Null-terminate the input */
+            return buffer; /* Return the input line */
         }
 
-	 char *token = strtok(input, " ");
-        char *args[10]; /* You can adjust the size as needed for your use case */
+        buffer[index] = c; /* Store the character in the buffer */
+        index++;
+    }
+}
 
-        int argCount = 0;
-        while (token != NULL)
+int main(void)
+{
+    char *input;
+    while (1)
+    {
+        printf("$ "); /* Display the shell prompt */
+        input = custom_getline(); /* Read a line of input */
+
+        if (input[0] == '\0') /* Check for empty input and ignore it */
+            continue;
+
+        if (strcmp(input, "exit") == 0)
         {
-            args[argCount] = token;
-            argCount++;
-            token = strtok(NULL, " ");
+            /* Implement the "exit" built-in */
+            free(input); /* Free the memory used for input */
+            exit(0); /* Exit the shell */
         }
-
-        args[argCount] = NULL; /* Null-terminate the argument list */
-
-
-        input[characters - 1] = '\0'; /* Remove the trailing newline character */
-
-        pid_t child_pid = fork();
-
-        if (child_pid == -1)
+        else if (strcmp(input, "env") == 0)
         {
-            perror("Fork error");
-            free(input);
-            exit(1);
-        }
-
-        if (child_pid == 0)
-        {
-            /* Child process */
-            char *args[2];
-            args[0] = input;
-            args[1] = NULL;
-
-            if (execve(args[0], args, NULL) == -1)
+            /* Implement the "env" built-in */
+            extern char **environ; /* Access the environment variables */
+            for (char **env = environ; *env; ++env)
             {
-                perror("Error");
-                free(input);
-                exit(1);
+                printf("%s\n", *env); /* Print each environment variable */
             }
         }
         else
         {
-            /* Parent process */
-            int status;
-            waitpid(child_pid, &status, 0); /* Wait for the child process to finish */
+            /* Handle non-built-in commands */
+            pid_t child_pid = fork(); /* Fork a child process */
+
+            if (child_pid == -1) /* Check for fork errors */
+            {
+                perror("Fork error"); /* Print an error message */
+                free(input); /* Free the memory used for input */
+                exit(1); /* Exit the shell with an error status */
+            }
+
+            if (child_pid == 0) /* In the child process */
+            {
+                /* Execute the command with arguments */
+                char *args[10]; /* Array to store command and arguments */
+                args[0] = input; /* Set the command as the first argument */
+                args[1] = NULL; /* Null-terminate the argument list */
+
+                if (execvp(args[0], args) == -1)
+                {
+                    perror("Execution Error"); /* Print an error message if execution fails */
+                    free(input); /* Free the memory used for input */
+                    exit(1); /* Exit the child process with an error status */
+                }
+            }
+            else /* In the parent process */
+            {
+                int status;
+                waitpid(child_pid, &status, 0); /* Wait for the child process to finish */
+            }
         }
+
+        free(input); /* Free the memory used for input */
     }
 
-    free(input);
-    return (0);
+    return 0; /* Exit the shell with a success status */
 }
-
