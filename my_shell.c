11e2e38de16fccaead8aedc35220dc502f84c1d5
nooby6 <giftaudrine1@gmail.com>
2023-11-06 00:37:01 -0800

    "my_shell.c"

--- my_shell.c
+++ my_shell.c
@@ -1,102 +1,147 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-/**
- * Entry point
- * *custom_getline - Function to read a line of input 
- * Return 0: Success
- *
- */
+/* Function to read a line of input using dynamic memory allocation */
 char *custom_getline(void)
 {
-    static char buffer[1024]; /* Static buffer to read input */
-    memset(buffer, 0, sizeof(buffer)); /* Clear the buffer */
+    char *buffer = NULL;
+    size_t bufsize = 0;
 
-    char c;
-    int index = 0;
+    ssize_t characters = getline(&buffer, &bufsize, stdin);
 
-    /* Read characters until a newline or the end of the file */
-    while (1)
+    if (characters == -1)
     {
-        c = getchar();
-
-        if (c == EOF || c == '\n')
-        {
-            buffer[index] = '\0'; /* Null-terminate the input */
-            return buffer; /* Return the input line */
-        }
-
-        buffer[index] = c; /* Store the character in the buffer */
-        index++;
+        perror("Error reading input");
+        free(buffer);
+        exit(1);
     }
+
+    buffer[characters - 1] = '\0'; /* Remove the trailing newline character */
+
+    return buffer;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    char *input;
-    char *args[10]; /* Declare the args array here */
-    while (1)
+    if (argc > 1) /* Non-interactive mode */
     {
-        printf("/$ "); /* Display the shell prompt */
-        input = custom_getline(); /* Read a line of input */
-
-        if (input[0] == '\0') /* Check for empty input and ignore it */
-            continue;
-
-        if (strcmp(input, "exit") == 0)
+        FILE *input_file = fopen(argv[1], "r");
+        if (input_file == NULL)
         {
-            /* Implement the "exit" built-in */
-            free(input); /* Free the memory used for input */
-            exit(0); /* Exit the shell */
+            perror("File open error");
+            exit(1);
         }
-        else if (strcmp(input, "env") == 0)
-        {
-            /* Implement the "env" built-in */
-            extern char **environ; /* Access the environment variables */
-            for (char **env = environ; *env; ++env)
-            {
-                printf("%s\n", *env); /* Print each environment variable */
-            }
-        }
-        else
-        {
-            /* Handle non-built-in commands */
-            pid_t child_pid = fork(); /* Fork a child process */
 
-            if (child_pid == -1) /* Check for fork errors */
+        char *input = NULL;
+        size_t bufsize = 0;
+        ssize_t characters;
+
+        while ((characters = getline(&input, &bufsize, input_file)) != -1)
+        {
+            input[characters - 1] = '\0'; /* Remove the trailing newline character */
+
+            /* Handle non-interactive shell logic */
+
+            pid_t child_pid = fork();
+
+            if (child_pid == -1)
             {
-                perror("Fork error"); /* Print an error message */
-                free(input); /* Free the memory used for input */
-                exit(1); /* Exit the shell with an error status */
+                perror("Fork error");
+                free(input);
+                exit(1);
             }
 
-            if (child_pid == 0) /* In the child process */
+            if (child_pid == 0)
             {
-                /* Execute the command with arguments */
-                char *args[10]; /* Array to store command and arguments */
-                args[0] = input; /* Set the command as the first argument */
-                args[1] = NULL; /* Null-terminate the argument list */
+                char *args[10];
+                args[0] = input;
+                args[1] = NULL;
 
                 if (execvp(args[0], args) == -1)
                 {
-                    perror("Execution Error"); /* Print an error message if execution fails */
-                    free(input); /* Free the memory used for input */
-                    exit(1); /* Exit the child process with an error status */
+                    perror("Execution Error");
+                    free(input);
+                    exit(1);
                 }
             }
-            else /* In the parent process */
+            else
             {
                 int status;
-                waitpid(child_pid, &status, 0); /* Wait for the child process to finish */
+                waitpid(child_pid, &status, 0);
             }
         }
 
-        free(input); /* Free the memory used for input */
+        free(input);
+        fclose(input_file);
+    }
+    else /* Interactive mode */
+    {
+        char *input;
+	char *args[10];
+
+        while (1)
+        {
+            printf("($) "); /* Display the shell prompt */
+            input = custom_getline(); /* Read a line of input */
+
+            if (input[0] == '\0') /* Check for empty input and ignore it */
+                continue;
+
+            /* Handle interactive shell logic */
+
+            if (strcmp(input, "exit") == 0)
+	    {
+                free(input);
+		exit(0);
+            }
+            else if (strcmp(input, "env") == 0)
+            {
+                extern char **environ;
+                char **env = environ;
+                while (*env)
+                {
+                    printf("%s\n", *env);
+                    env++;
+                }
+            }
+            else
+            {
+                pid_t child_pid = fork();
+
+                if (child_pid == -1)
+                {
+                    perror("Fork error");
+                    free(input);
+                    exit(1);
+                }
+
+                if (child_pid == 0)
+                {
+                    args[0] = input;
+                    args[1] = NULL;
+
+                    if (execvp(args[0], args) == -1)
+                    {
+                        perror("Execution Error");
+                        free(input);
+                        exit(1);
+                    }
+                }
+                else
+                {
+                    int status;
+                    waitpid(child_pid, &status, 0);
+                }
+            }
+
+            free(input);
+        }
     }
 
-    return 0; /* Exit the shell with a success status */
+    return 0;
 }
+
